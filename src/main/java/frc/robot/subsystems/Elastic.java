a0ce66c74e0c68bc74cdf9c5916ca2dc4377b4b6
Turkey5 <turkteq@gmail.com>
2026-04-03 10:53:42 -0500

    Added overloaded constructors to Elastic class
    Added overloaded constructors for Elastic class to support both int and double types.

--- src/main/java/frc/robot/subsystems/Elastic.java
+++ src/main/java/frc/robot/subsystems/Elastic.java
@@ -1,5 +1,18 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 public class Elastic extends SubsystemBase {
-  
+  public Elastic() {
+  }
+
+  public Elastic(int baseValue, String keyName) {
+    //creates an elastic display/input for an int with the name keyName
+  }
+
+  public Elastic(double baseValue, Sting keyName) {
+    //creates an elastic display/input for a double with the name keyName
+  }
 }
+
